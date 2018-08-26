dc93b370fe813f964057fff45f091421684b4572
science162 <albertomouaffo@gmail.com>
2018-08-27 01:41:10 +0200

    new_map

--- src/pages/details/details.ts
+++ src/pages/details/details.ts
@@ -1,6 +1,6 @@
 import { Component} from '@angular/core';
 import { Platform } from 'ionic-angular';
-import { GoogleMaps, GoogleMap,GoogleMapOptions,GoogleMapsEvent} from '@ionic-native/google-maps';
+import { GoogleMaps, GoogleMap,GoogleMapOptions,GoogleMapsEvent, Marker,} from '@ionic-native/google-maps';
 
 @Component({
   selector: 'page-details',
@@ -9,33 +9,40 @@ import { GoogleMaps, GoogleMap,GoogleMapOptions,GoogleMapsEvent} from '@ionic-na
 
 export class DetailsPage {
 
-  map: GoogleMap ;
-  mapElement: HTMLElement;
+  map: GoogleMap;
+  constructor() { }
 
-
-  constructor(private googleMaps: GoogleMaps, public platform:Platform) {
-
+  ionViewDidLoad() {
+    this.loadMap();
   }
 
   loadMap() {
-    this.mapElement = document.getElementById('map');
-    let mapOption : GoogleMapOptions = {
-      camera:{
-        target:
-        {
-          lat:43.0741904,
-          lng:-89.3809802
-        },
-        zoom: 18,
-        tilt:30
-      }
-    }
-    this.map = GoogleMaps.create(this.mapElement, mapOption);
-    this.map.one(GoogleMapsEvent.MAP_READY).then(
-      () => {
-        alert("Map is ready to be used")
-      }
-    )
-  }
 
+    let mapOptions: GoogleMapOptions = {
+      camera: {
+         target: {
+           lat: 43.0741904,
+           lng: -89.3809802
+         },
+         zoom: 18,
+         tilt: 30
+       }
+    };
+
+    this.map = GoogleMaps.create('map', mapOptions);
+
+    let marker: Marker = this.map.addMarkerSync({
+      title: 'Ionic',
+      icon: 'blue',
+      animation: 'DROP',
+      position: {
+        lat: 43.0741904,
+        lng: -89.3809802
+      }
+    });
+    marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
+      alert('clicked');
+    });
+  }
 }
+
